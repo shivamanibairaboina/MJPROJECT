47e6f9022a4ae69cfccef48c66ade192c1c9d38a
shivamani <shivamanibairaboina@gmail.com>
2024-11-15 22:36:29 +0530

    First commit to maven project

---
+++ src/main/java/SEgroup/batch2java/App.java
@@ -0,0 +1,13 @@
+package SEgroup.batch2java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Iam B.shivamani -23BD5A6613"  );
+    }
+}
